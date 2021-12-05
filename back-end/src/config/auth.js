2d69c3e710e5b69743d10e884ce679aacc04b3dc
michael23-lopes <michael23.wellington@gmail.com>
2021-12-05 11:46:39 -0300

    configuração de projeto, criação e edição de usuários, sessão de usuário e validação dos dados

---
+++ back-end/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3b6e80fb7e595f4d9898cd60d2605cdb',
+  expiresIn: '7d',
+};
---
+++ back-end/src/server.js
@@ -0,0 +1,5 @@
+import app from './app';
+
+app.listen(3333, function () {
+  console.log('Server running at http://localhost:3333');
+});
